4d417df9904d514bca458362108ee9ee3b36633f
3elc <99827130+3elc@users.noreply.github.com>
2024-01-25 13:20:50 +0200

    Update Python_Functions.py

--- Python_Functions.py
+++ Python_Functions.py
@@ -1,12 +1,12 @@
-import re
-# Function to replace markdown links with their names in text
 def md_input(path: str):
-"""blabla docstring
+"""blabla Function to replace markdown links with their names in text
 :type
 """"
+  import re
   with open(path, "r") as mf:
     file = mf.readlines()
   return 
+
 def replace_links_with_names(text):
     # Define the regular expression pattern for a Markdown link
     pattern = r'\[([^]]+)\]\(([^)]+)\)'
@@ -21,13 +21,29 @@ def replace_links_with_names(text):
     replaced_text = re.sub(pattern, replace_link, text)
     
     return replaced_text
+
 def md_input(path):
   :type
   with open(path, "w") as mf:
     mf.write(new_text)
+
+
+
+def decrypt_pdfs(paths,
+  from pypdf import PdfWriter, PdfReader
+  PASSLIST = input("enter path: ")
+  for file in paths:
+    file = PdfReader(file)
+    if file.isEncrypted:
+      content = PdfWriter()
+      for i in file.pages:
+        content.addpage(i)
+      for i in "blabla","123","5453":       
+        pass
+        with open(file, "wb") as out_stream: pass
+
 main():
   markdown_text = """"""
-
 # Replace links with their names while preserving the text structure
 new_text = replace_links_with_names(markdown_text)
 with open("bookmarks_strings.md", "w") as mf:
